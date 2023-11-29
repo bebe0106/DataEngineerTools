c4e72dbfeefa4c185d372520e626fe1e4d01edd6
=Benoit Marchadier <marchadier.benoit@gmail.com>
2023-11-29 14:42:57 +0100

    progression

--- 2Scrapy/newscrawler/newscrawler/spiders/lemonde_v2.py
+++ 2Scrapy/newscrawler/newscrawler/spiders/lemonde_v2.py
@@ -18,4 +18,17 @@ class LemondeSpider(scrapy.Spider):
             "title":title,
             "all_links":all_links
         }
-        
+
+    def parse_category(self, response):
+        for article in response.css(".river")[0].css(".teaser"):
+            title = self.clean_spaces(article.css("h3::text").extract_first())
+            image = article.css("img::attr(data-src)").extract_first()
+            description = article.css("p::text").extract_first()
+            yield {
+                "title": title,
+                "image": image,
+                "description": description
+            }
+    def clean_spaces(string_):
+        if string_ is not None:
+            return " ".join(string_.split())
